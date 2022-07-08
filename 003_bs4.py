2b976567ba1e0f12e2bd740771332732e99fcfa3
Yaroslav Baienko <zerhug@gmail.com>
2022-07-08 21:04:00 +0300

    coursera

--- 003_bs4.py
+++ 003_bs4.py
@@ -15,6 +15,13 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+url = input('Enter link: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
 
 
 
