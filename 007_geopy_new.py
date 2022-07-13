7d3471e5681379623d2eac85f0213f11ee274986
citizenfour <zerhug@gmail.com>
2022-07-13 16:21:12 +0300

    onubuntu

--- 007_geopy_new.py
+++
@@ -1,8 +0,0 @@
-from geopy import GoogleV3
-
-place = input("Enter address: ")
-
-location = GoogleV3(api_key='AIzaSyCl9k2oHhGfRqwpj3Zdog1u_Hzimqgmca8').geocode(place)
-
-print(location.address)
-print(location)
